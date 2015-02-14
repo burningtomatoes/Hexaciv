6201eba66426c60a8c0229cafae8da380e4a5cbe
Roy de Jong <roy@softwarepunt.nl>
2015-02-14 20:09:52 +0100

    Add better cursor design, do not render cursor until a mouse event is observed

--- src/input/mouse.js
+++ src/input/mouse.js
@@ -2,21 +2,30 @@ var Mouse = {
     posX: 0,
     posY: 0,
 
+    wasSet: false,
+
     imgDefault: null,
 
     w: 16,
     h: 16,
 
     init: function () {
+        this.wasSet = false;
+
         $('body').mousemove(function (e) {
             this.posX = e.pageX;
             this.posY = e.pageY;
+            this.wasSet = true;
         }.bind(this));
 
         this.imgDefault = Game.images.load('cursor_default.png');
     },
 
     draw: function (ctx) {
+        if (!this.wasSet) {
+            return;
+        }
+
         ctx.drawImage(this.imgDefault, 0, 0, this.w, this.h, Math.round(this.posX / Canvas.upscaleRatio), Math.round(this.posY / Canvas.upscaleRatio), this.w, this.h);
     }
 };
